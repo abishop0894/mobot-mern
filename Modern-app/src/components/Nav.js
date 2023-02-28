b6a8e6c834259c28bfd1f5ce0d6b04efef50d8e3
alex <abishop0831@gmail.com>
2023-02-28 09:47:02 -0500

    updates

--- Modern-app/src/components/Nav.js
+++ Modern-app/src/components/Nav.js
@@ -145,7 +145,7 @@ const Nav = () => {
               transition={{ ease: "easeIn", duration: 0.9 }}
               style={{
                 position: "relative",
-                bottom: "-6vh",
+                bottom: "-5vh",
               }}
             >
               <p
